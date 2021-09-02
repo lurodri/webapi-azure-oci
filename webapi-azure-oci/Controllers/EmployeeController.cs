44906a0332781f19cb491e9c633a974cf99c6e04
Luciano Silva <lurodrisilva@hotmail.com>
2021-09-02 20:19:06 +0000

    adding dependency tracker

--- webapi-azure-oci/Controllers/EmployeeController.cs
+++ webapi-azure-oci/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Oracle.ManagedDataAccess.Client;
+using Microsoft.ApplicationInsights;
 
 namespace webapi_azure_oci.Controllers
 {
@@ -12,17 +13,19 @@ namespace webapi_azure_oci.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
 
-        private readonly ILogger<EmployeeController> _logger;
+        //private readonly ILogger<EmployeeController> _logger;
 
-        public EmployeeController(ILogger<EmployeeController> logger)
-        {
-            _logger = logger;
-        }
+        //public EmployeeController(ILogger<EmployeeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private TelemetryClient telemetryClient = new TelemetryClient();
 
         [HttpGet]
         public IEnumerable<Employee> Get()
@@ -36,15 +39,18 @@ namespace webapi_azure_oci.Controllers
             //})
             //.ToArray();
 
-            string conString = "User Id=hr;Password=<password>;Data Source=<ip or hostname>:1521/<service name>;";
+            string conString = "User Id=poc;Password=poc_OCIAzure_2k21;Data Source=10.1.0.250:1521/db0901_pdb1.clientsubnet.vncdatabase.oraclevcn.com;";
             List<Employee> empList = new List<Employee>();
+
+            var startTime = DateTime.UtcNow;
+            var timer = System.Diagnostics.Stopwatch.StartNew();
 
             using (OracleConnection con = new OracleConnection(conString))
             {
                 using (OracleCommand cmd = con.CreateCommand())
                 {
-                    //try
-                    //{
+                    try
+                    {
                         con.Open();
                         cmd.BindByName = true;
 
@@ -53,11 +59,14 @@ namespace webapi_azure_oci.Controllers
                         cmd.CommandText = "select first_name, last_name from employees where department_id = :id";
 
                         // Assign id to the department number 50 
-                        OracleParameter id = new OracleParameter("id", 50);
+                        OracleParameter id = new OracleParameter("id", 100);
                         cmd.Parameters.Add(id);
 
                         //Execute the command and use DataReader to display the data
                         OracleDataReader reader = cmd.ExecuteReader();
+                        timer.Stop();
+                        telemetryClient.TrackDependency("OracleRequest", "OracleQuery", "OracleQueryStatus", startTime, timer.Elapsed, true);
+
                         while (reader.Read())
                         {
                             empList.Add(new Employee()
@@ -67,18 +76,22 @@ namespace webapi_azure_oci.Controllers
                             }
                             );
                         }
-                    return empList.ToArray();
-                    reader.Dispose();
-                    //}
-                    //catch (Exception ex)
-                    //{
-                    //    //await context.Response.WriteAsync(ex.Message);
-                    //    return Enumerable.Range(1, 1).Select(index => new Employee
-                    //    {
-                    //        FirstName = null,
-                    //        LastName = null
-                    //    }).ToArray();
-                    //}
+                        return empList.ToArray();
+                    }
+                    catch (Exception ex)
+                    {
+                        //await context.Response.WriteAsync(ex.Message);
+                        empList.Add(new Employee()
+                        {
+                            FirstName = null,
+                            LastName = null
+                        });
+                        return empList.ToArray();
+                    }
+                    finally
+                    {
+
+                    }
                 }
             }
         }
