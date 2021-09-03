4e02b80aebb10d65bde0dfd925e5f76ba5ad1ae6
Luciano Rodrigues <lurodri@microsoft.com>
2021-09-03 18:05:59 -0300

    changing dependency tracker

--- webapi-azure-oci/Controllers/EmployeeController.cs
+++ webapi-azure-oci/Controllers/EmployeeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Oracle.ManagedDataAccess.Client;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace webapi_azure_oci.Controllers
 {
@@ -13,86 +14,73 @@ namespace webapi_azure_oci.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
-        //private static readonly string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
 
-        //private readonly ILogger<EmployeeController> _logger;
-
-        //public EmployeeController(ILogger<EmployeeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        private TelemetryClient telemetryClient = new TelemetryClient();
+        private readonly TelemetryClient telemetryClient = new TelemetryClient();
 
         [HttpGet]
         public IEnumerable<Employee> Get()
         {
-            //var rng = new Random();
-            //return Enumerable.Range(1, 5).Select(index => new Employee
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //})
-            //.ToArray();
 
             string conString = "User Id=poc;Password=poc_OCIAzure_2k21;Data Source=10.1.0.250:1521/db0901_pdb1.clientsubnet.vncdatabase.oraclevcn.com;";
             List<Employee> empList = new List<Employee>();
 
-            var startTime = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-
-            using (OracleConnection con = new OracleConnection(conString))
+            using OracleConnection con = new OracleConnection(conString);
+            using OracleCommand cmd = con.CreateCommand();
+            try
             {
-                using (OracleCommand cmd = con.CreateCommand())
+
+                // Establish an operation context and associated telemetry item:
+                using (var operation = telemetryClient.StartOperation<RequestTelemetry>("OracleRequest"))
                 {
-                    try
+                    // Telemetry sent in here will use the same operation ID.
+                    telemetryClient.TrackTrace("OracleQuery"); // or other Track* calls
+
+                    con.Open();
+                    cmd.BindByName = true;
+
+                    //Use the command to display employee names from 
+                    // the EMPLOYEES table
+                    cmd.CommandText = "select first_name, last_name from employees where department_id = :id";
+
+                    // Assign id to the department number 50 
+                    OracleParameter id = new OracleParameter("id", 100);
+                    cmd.Parameters.Add(id);
+
+                    //Execute the command and use DataReader to display the data
+                    OracleDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
                     {
-                        con.Open();
-                        cmd.BindByName = true;
-
-                        //Use the command to display employee names from 
-                        // the EMPLOYEES table
-                        cmd.CommandText = "select first_name, last_name from employees where department_id = :id";
-
-                        // Assign id to the department number 50 
-                        OracleParameter id = new OracleParameter("id", 100);
-                        cmd.Parameters.Add(id);
-
-                        //Execute the command and use DataReader to display the data
-                        OracleDataReader reader = cmd.ExecuteReader();
-                        timer.Stop();
-                        telemetryClient.TrackDependency("OracleRequest", "OracleQuery", "OracleQueryStatus", startTime, timer.Elapsed, true);
-
-                        while (reader.Read())
-                        {
-                            empList.Add(new Employee()
-                            {
-                                FirstName = reader.GetString(0),
-                                LastName = reader.GetString(1)
-                            }
-                            );
-                        }
-                        return empList.ToArray();
-                    }
-                    catch (Exception ex)
-                    {
-                        //await context.Response.WriteAsync(ex.Message);
                         empList.Add(new Employee()
                         {
-                            FirstName = null,
-                            LastName = null
-                        });
-                        return empList.ToArray();
+                            FirstName = reader.GetString(0),
+                            LastName = reader.GetString(1)
+                        }
+                        );
                     }
-                    finally
-                    {
 
-                    }
-                }
+                    // Set properties of containing telemetry item--for example:
+                    operation.Telemetry.ResponseCode = "200";
+
+                    // Optional: explicitly send telemetry item:
+                    telemetryClient.StopOperation(operation);
+
+                } // When operation is disposed, telemetry item is sent.
+                return empList.ToArray();
+            }
+            catch (Exception ex)
+            {
+                //await context.Response.WriteAsync(ex.Message);
+                empList.Add(new Employee()
+                {
+                    FirstName = null,
+                    LastName = null
+                });
+                return empList.ToArray();
+            }
+            finally
+            {
+
             }
         }
     }
