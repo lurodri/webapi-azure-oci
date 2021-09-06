7204545be09190f7ee79eaee2b5cc3182f303297
Luciano Rodrigues <lurodri@microsoft.com>
2021-09-06 17:49:36 -0300

    removing flush from app insights

--- webapi-azure-oci/Controllers/EmployeeController.cs
+++ webapi-azure-oci/Controllers/EmployeeController.cs
@@ -17,7 +17,7 @@ namespace webapi_azure_oci.Controllers
     {
         private TelemetryClient telemetryClient;
         
-        public >EmployeeController()
+        public EmployeeController()
         {
             TelemetryConfiguration configuration = new TelemetryConfiguration();
             configuration.InstrumentationKey = "6bca0969-e2e2-4dbe-914a-5aa25a48f56c";
