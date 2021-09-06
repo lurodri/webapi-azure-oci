f08ab1076ae7f034d68344ea2a246e7cba8085ac
Luciano Rodrigues <lurodri@microsoft.com>
2021-09-06 13:36:18 -0300

    fixing odp.net consumption

--- webapi-azure-oci/Controllers/EmployeeController.cs
+++ webapi-azure-oci/Controllers/EmployeeController.cs
@@ -50,52 +50,57 @@ namespace webapi_azure_oci.Controllers
 
             string conString = "User Id=poc;Password=poc_OCIAzure_2k21;Data Source=10.1.0.250:1521/db0901_pdb1.clientsubnet.vncdatabase.oraclevcn.com;";
             List<Employee> empList = new List<Employee>();
-            
-            using OracleConnection con = new OracleConnection(conString);
-            using OracleCommand cmd = con.CreateCommand();
-            try
+
+            using (OracleConnection con = new OracleConnection(conString))
             {
-                using (var operation = telemetryClient.StartOperation<DependencyTelemetry>("Oracle Request"))
+                using (OracleCommand cmd = con.CreateCommand())
                 {
-                    con.Open();
-                    cmd.BindByName = true; 
-
-                    //Use the command to display employee names from 
-                    // the EMPLOYEES table
-                    cmd.CommandText = "select first_name, last_name from employees where department_id = :id";
-
-                    // Assign id to the department number 50 
-                    OracleParameter id = new OracleParameter("id", 100);
-                    cmd.Parameters.Add(id);
-
-                    //Execute the command and use DataReader to display the data
-                    OracleDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
+                    try
                     {
+                        using (var operation = telemetryClient.StartOperation<DependencyTelemetry>("Oracle Request"))
+                        {
+                            con.Open();
+                            cmd.BindByName = true;
+
+                            //Use the command to display employee names from 
+                            // the EMPLOYEES table
+                            cmd.CommandText = "select first_name, last_name from employees where department_id = :id";
+
+                            // Assign id to the department number 50 
+                            OracleParameter id = new OracleParameter("id", 100);
+                            cmd.Parameters.Add(id);
+
+                            //Execute the command and use DataReader to display the data
+                            OracleDataReader reader = cmd.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                empList.Add(new Employee()
+                                {
+                                    FirstName = reader.GetString(0),
+                                    LastName = reader.GetString(1)
+                                }
+                                );
+                            }
+                            reader.Dispose();
+                        }
+                        return empList.ToArray();
+                    }
+                    catch (Exception ex)
+                    {
+                        //await context.Response.WriteAsync(ex.Message);
                         empList.Add(new Employee()
                         {
-                            FirstName = reader.GetString(0),
-                            LastName = reader.GetString(1)
-                        }
-                        );
+                            FirstName = null,
+                            LastName = null
+                        });
+                        return empList.ToArray();
+                    }
+                    finally
+                    {
+
                     }
                 }
-                return empList.ToArray();
-            }
-            catch (Exception ex)
-            {
-                //await context.Response.WriteAsync(ex.Message);
-                empList.Add(new Employee()
-                {
-                    FirstName = null,
-                    LastName = null
-                });
-                return empList.ToArray();
-            }
-            finally
-            {
-
             }
         }
     }
